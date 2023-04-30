bba9e1ad9fb01e80e6e0b90d5ba834da997037c1
Jake Soenneker <jake@soenneker.com>
2023-04-29 20:34:04 -0500

    Initial commit

---
+++ src/Registrars/RedisSuiteRegistrar.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Soenneker.Redis.Lock.Registrars;
+using Soenneker.Redis.Util.Server.Registrars;
+
+namespace Soenneker.Redis.Suite.Registrars;
+
+/// <summary>
+/// A concoction of Redis utilities and libraries
+/// </summary>
+public static class RedisSuiteRegistrar
+{
+    public static void AddRedisSuiteAsSingleton(this IServiceCollection services)
+    {
+        services.AddRedisLockUtilAsSingleton();
+        services.AddRedisServerUtilAsSingleton();
+    }
+}
